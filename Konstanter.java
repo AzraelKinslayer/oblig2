9b95fa2a8deaedac8023ccfa49c342cf3b5f1b46
AzraelKinslayer <jakob.prescher@yahoo.com>
2016-02-26 22:31:48 +0100

    Added files via upload

---
+++ Konstanter.java
@@ -0,0 +1,22 @@
+package oblig2;
+
+public interface Konstanter {
+
+void settFornavn(String fornavn);
+
+	String hentForNavn();
+
+	void settEtterNavn(String etterNavn);
+
+	String hentEtterNavn();
+
+	void settFulltNavn(String forNavn, String etterNavn);
+
+	String hentFulltNavn();
+
+	double beregnKreditt();
+
+	double beregnBonus();
+}
+
+
